fd109467b152a0078bff3109e460116abecd201c
Daniel Rice <dlrice@ebi.ac.uk>
2019-09-03 14:32:22 +0100

    Initial commit.

---
+++ ebi25-print-ticket.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import sys
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from jinja2 import Template
+import pdfkit
+import os
+import uuid
+import json
+import pathlib
+
+app = Flask(__name__)
+CORS(app)
+
+TMP = '~/ebi25/tmp'
+pathlib.Path(TMP).mkdir(parents=True, exist_ok=True)
+
+
+template = Template('''
+<html>
+  <head>
+    <style>
+      body {
+        font-size: 40px;
+        padding-left: 18px;
+      }
+    </style>
+  </head>
+  <body>
+    {{ number }}
+  </body>
+</html>
+''')
+
+
+def send_pdf_to_printer(pdf_path):
+    command = f'lp {pdf_path}'
+    os.system(command)
+
+
+def generate_pdf(number):
+    filename = uuid.uuid4().hex
+    html_path = os.path.join(TMP, f'{filename}.html')
+    pdf_path = os.path.join(TMP, f'{filename}.pdf')
+    with open(html_path, 'w') as f:
+        f.write(template.render(number=number))
+    pdfkit.from_file(html_path, pdf_path)
+    return pdf_path
+
+
+def success():
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+def failure(exception):
+    return json.dumps({'success': False, 'exception': exception}), 400, {'ContentType': 'application/json'}
+
+
+@app.route("/<number>/")
+def print_number(number):
+    try:
+        pdf_path = generate_pdf(number)
+        send_pdf_to_printer(pdf_path)
+    except Exception as e:
+        return failure(e)
+    else:
+        return success()
